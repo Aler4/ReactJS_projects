e48283bb1e5621c9e061b4a49a0c002b845b3505
Aler4 <alerch5666@gmail.com>
2023-01-23 19:01:52 +0300

    Revert "add data create layout"
    This reverts commit 180c315f094e1f4d75b47b990f7bec155e98d956.

--- src/redux/actions.ts
+++
@@ -1,8 +0,0 @@
-import {TQuestion} from "./reducer";
-
-
-export const ADD_QUESTIONS = 'ADD_QUESTIONS';
-export const UPLOAD_QUESTIONS = 'UPLOAD_QUESTIONS';
-
-export const addQuestions = (data: TQuestion[]) => ({data, type: ADD_QUESTIONS});
-const uploadQuestions = () => ({type: UPLOAD_QUESTIONS});
--- src/redux/selectors.ts
+++
@@ -1,7 +0,0 @@
-import {TState} from "./reducer";
-
-export const questionsSelector = (state: TState) => {
-   let  res = state.questions.length > 0 ? (state.questions.sort(() => Math.random() - 0.5)).splice(0, state.questions.length / 5)
-       : state.questions;
-    return res.sort(() => Math.random() - 0.5);
-}
